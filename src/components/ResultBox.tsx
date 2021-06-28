22457adb25dbfb51ffc5c207a5ce7d041bd1c708
Armando <jaimes087.uz@gmail.com>
2021-06-28 11:27:27 -0500

    Copy to clipboard functionality

--- src/components/ResultBox.tsx
+++ src/components/ResultBox.tsx
@@ -1,17 +1,25 @@
 import { MdContentCopy, MdVolumeUp } from 'react-icons/md';
 import { TranslateBoxProps } from '../constants/languages';
+import * as clipboard from '../helpers/clipboard';
 import TextBox, { Actions, TextArea } from './TextBox';
 import BtnIcon from './BtnIcon';
+import { useRef } from 'react';
 
 function ResultBox({ language }: TranslateBoxProps) {
+	const text: string =
+		'Hola a todos como están yo me encuentro muy bien gracias';
+
+	const textAreaRef = useRef<HTMLTextAreaElement>(null!);
+
 	return (
 		<TextBox>
-			<TextArea />
+			<TextArea ref={textAreaRef} defaultValue={text} readOnly={true} />
 			<Actions>
 				<BtnIcon aria-label="Listen" data-title="Listen">
 					<MdVolumeUp size="24" color="currentColor" />
 				</BtnIcon>
 				<BtnIcon
+					onClick={() => clipboard.copy(text, textAreaRef.current)}
 					aria-label="Copy translation"
 					data-title="Copy translation"
 				>
--- src/helpers/speech.ts
+++
@@ -1,1 +0,0 @@
-export const cancelSpeechSynthesis = () => window.speechSynthesis.cancel();
